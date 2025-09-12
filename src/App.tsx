9bcc88c946e32277c3ff885fbe83519641535c55
Huangrui47 <1477315575@qq.com>
2025-09-12 15:13:47 +0800

    navbar完成

--- src/App.tsx
+++ src/App.tsx
@@ -1,54 +1,63 @@
 import React from 'react'
 import { Anchor, ConfigProvider } from 'antd';
 import { navLinks } from '../constants/index';
-
+import Logo from '../src/asset/images/logo.png'
 
 export default function App() {
 
 
   return (
     <>
-      <ConfigProvider
-        theme={{
-          components: {
-            Anchor: {
-              colorText: 'white',
-              colorPrimary: 'white',
-              colorPrimaryHover: 'white',
-              colorPrimaryActive: 'white',
-              colorTextSecondary: 'white',
-              colorTextTertiary: 'white',
+      <div className="flex justify-between items-center p-8 bg-[black] fixed top-0 left-0 right-0 z-50">
+        <div className="flex items-center gap-4">
+          <img src={Logo} alt="" width={40} height={40} />
+          <p className="text-white font-bold text-xl" style={{ fontFamily: '"Modern Negra", sans-serif' }} >Velvet Pour</p>
+        </div>
+
+        <ConfigProvider
+          theme={{
+            components: {
+              Anchor: {
+                colorText: 'white',
+                colorPrimary: 'white',
+                colorPrimaryHover: 'white',
+                colorPrimaryActive: 'white',
+                colorTextSecondary: 'white',
+                colorTextTertiary: 'white',
+              }
             }
-          }
-        }}
-      >
-        <Anchor
-          direction="horizontal"
-          targetOffset={0}
-          style={{ paddingBlockStart: '44px', }}
-          className="flex justify-end bg-[black] p-8"
-          items={navLinks.map((link) => ({
-            key: link.id,
-            href: `#${link.id}`,
-            title: link.title,
-          }))}
-        /></ConfigProvider>
-      {
-        navLinks.map((link) => (
-          <div
-            key={link.id}
-            id={link.id}
-            style={{
-              width: '100vw',
-              height: '500px',
-              textAlign: 'center',
-              background: 'rgba(17, 236, 17, 0.5)',
-            }}
-          >
-           {React.createElement(link.component)}
-          </div>
-        ))
-      }
+          }}
+        >
+          <Anchor
+            direction="horizontal"
+            targetOffset={0}
+            className="flex justify-end bg-[black]"
+            items={navLinks.map((link) => ({
+              key: link.id,
+              href: `#${link.id}`,
+              title: link.title,
+            }))}
+          /></ConfigProvider>
+      </div>
+      <div className="mt-[120px]">
+        {
+          navLinks.map((link) => (
+            <div
+              key={link.id}
+              id={link.id}
+              style={{
+                width: '100vw',
+                height: '500px',
+                textAlign: 'center',
+                background: 'rgba(17, 236, 17, 0.5)',
+              }}
+            >
+              {React.createElement(link.component)}
+            </div>
+          ))
+        }
+      </div>
+
 
 
     </>
